ef72d2e490aee9a52f2bad80ff18b3664c655bbf
mike <mike.sk.traynor@gmail.com>
2024-02-12 11:09:18 -0400

    Change gruvbox comment color to blue

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -7,9 +7,10 @@ require("gruvbox").setup({
       comments = false,
       operators = false,
       folds = true,
-  },
+    },
+    overrides = {
+        Comment = {fg = "#83a598"} -- Gruvbox.pallet.bright_blue
+    }
 })
 -- Set the colorscheme
 vim.cmd("colorscheme gruvbox")
-
-
