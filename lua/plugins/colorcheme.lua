0ee0be72ca80f4716d072f0c4c3aebf6446e1a2e
Michael Traynor <michael.traynor@cerence.com>
2024-05-08 10:39:58 -0300

    colorscheme configured in same place they are loaded. Transparent Tokyonight.

--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,12 +1,37 @@
 return {
-    -- add gruvbox
-    { "ellisonleao/gruvbox.nvim" },
+    -- add cholorschemes
+    { "ellisonleao/gruvbox.nvim",
+      opts = {
+        transparent_mode = true,
+        italic = {
+          strings = false,
+          emphasis = true,
+          comments = false,
+          operators = false,
+          folds = true,
+        },
+        overrides = {
+          Comment = {fg = "#83a598"} -- Gruvbox.pallet.bright_blue
+        }
+      }
+    },
+    {"folke/tokyonight.nvim",
+      lazy = true,
+      opts = {
+        style = "night",
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+      }
+    },
 
-    -- Configure LazyVim to load gruvbox
+    },
+    -- Configure LazyVim to load a colorscheme
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "gruvbox",
+            colorscheme = "tokyonight",
         },
     },
 }
